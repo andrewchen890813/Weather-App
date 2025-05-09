e5c5a482d0c2fe0f239827405b673eb8c7e0ccd8
andrewchen890813 <k0919042712@gmail.com>
2025-05-09 14:26:47 +0800

    建立api

---
+++ src/api/weather.ts
@@ -0,0 +1,63 @@
+import { API_CONFIG } from "./config";
+import {
+  Coordinates,
+  ForecastData,
+  GeocodingResponse,
+  WeatherData,
+} from "./types";
+
+class WeatherAPI {
+  // 組出API網址
+  private createUrl(endpoint: string, params: Record<string, string | number>) {
+    const searchParams = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+    return `${endpoint}?${searchParams.toString()}`;
+  }
+
+  //   發送請求
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Weather API Error:${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  //   目前天氣
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.unites,
+    });
+    return this.fetchData<WeatherData>(url);
+  }
+
+  //   未來天氣
+  async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.unites,
+    });
+    return this.fetchData<ForecastData>(url);
+  }
+
+  // 地理編碼
+  async reverseGeocode({
+    lat,
+    lon,
+  }: Coordinates): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      limit: 1,
+    });
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+}
+
+export const weatherAPI = new WeatherAPI();
