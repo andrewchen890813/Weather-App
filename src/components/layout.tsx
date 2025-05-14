c8e597808ad55150c522a29e51188b60c8786e7c
andrewchen890813 <k0919042712@gmail.com>
2025-05-14 22:24:18 +0800

    footer 修改

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ const layout = ({ children }: PropsWithChildren) => {
       </main>
       <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-          <p>Andrew Chen</p>
+          <p>Made with ❤️ by Andrew Chen</p>
         </div>
       </footer>
     </div>
